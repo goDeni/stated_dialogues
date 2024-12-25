6fd72c563ead40660b8df32cedd893392c447cb5
Denis <den4ikept11@yandex.ru>
2024-12-25 14:59:41 +0500

    Update libs, added document sending feature (#5)

--- src/dialogues/mod.rs
+++ src/dialogues/mod.rs
@@ -9,6 +9,18 @@ pub enum MessageFormat {
 }
 
 #[derive(Debug, PartialEq)]
+pub struct OutgoingDocument {
+    pub data: Vec<u8>,
+    pub name: String,
+}
+
+impl OutgoingDocument {
+    pub fn new(data: Vec<u8>, name: String) -> Self {
+        OutgoingDocument { data, name }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub struct OutgoingMessage {
     text: String,
     pub format: MessageFormat,
@@ -43,6 +55,7 @@ impl From<OutgoingMessage> for String {
 
 pub enum CtxResult {
     Messages(Vec<OutgoingMessage>),
+    Document(OutgoingDocument),
     RemoveMessages(Vec<MessageId>),
     Buttons(OutgoingMessage, Vec<Vec<(ButtonPayload, String)>>),
     NewCtx(Box<dyn DialContext + Send + Sync + 'static>),
@@ -54,6 +67,7 @@ impl Debug for CtxResult {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Messages(arg0) => f.debug_tuple("Messages").field(arg0).finish(),
+            Self::Document(arg0) => f.debug_tuple("Document").field(arg0).finish(),
             Self::Buttons(arg0, arg1) => f.debug_tuple("Buttons").field(arg0).field(arg1).finish(),
             Self::NewCtx(_) => f.debug_tuple("NewCtx(?)").finish(),
             Self::Nothing => write!(f, "Nothing"),
